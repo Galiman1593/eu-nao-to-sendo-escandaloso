2ed66853fb15b462dce6bad7952c8a9c3064ffef
Galiman1593 <eunesiopinto01@gmail.com>
2024-09-27 21:35:35 -0300

    feat:primeiro_passo_jungle

--- projetoDjango/cadastro/admin.py
+++ projetoDjango/cadastro/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Curso
+from .models import Alunos
+from .models import Professores
 # Register your models here.
+admin.site.register(Curso)
+admin.site.register(Alunos)
+admin.site.register(Professores)
